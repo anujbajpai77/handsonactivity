8ed90ec15a3445cfeccb37f03fb5d6d97b847fab
w2cluster238 <w2cluster238@w2cluster291.iiht.tech>
2020-06-01 12:04:03 +0530

    Dockerize the microservices.

--- ConvertCurrency/src/main/java/com/ibm/activity/convertcurrency/client/ConvertCurrencyRestClient.java
+++ ConvertCurrency/src/main/java/com/ibm/activity/convertcurrency/client/ConvertCurrencyRestClient.java
@@ -1,6 +1,5 @@
 package com.ibm.activity.convertcurrency.client;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
