a72edf514187cfc1d216a6d8315753020e66e9a2
w2cluster238 <w2cluster238@w2cluster291.iiht.tech>
2020-06-03 01:13:50 +0530

    JWT token creation and mapping with all rest end points.

---
+++ AccountLoginService/src/main/java/com/ibm/activity/accountloginservice/security/config/AccountLoginSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.ibm.activity.accountloginservice.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.ibm.activity.accountloginservice.filter.JwtRequestFilter;
+
+@EnableWebSecurity
+public class AccountLoginSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	UserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// auth.inMemoryAuthentication().withUser("anuj").password("anuj").roles("Admin").and().withUser("blah")
+		// .password("blah").roles("User");
+
+		auth.userDetailsService(userDetailsService);
+	}
+
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		
+	httpSecurity.authorizeRequests()
+		.antMatchers("/console/**").hasRole("Admin").and().formLogin().and().csrf()
+		.ignoringAntMatchers("/h2-console/**").and().headers().frameOptions().sameOrigin().disable()
+		.authorizeRequests().antMatchers("/loginservice/authenticate").permitAll().anyRequest().authenticated()
+		.and().exceptionHandling().and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	
+	    httpSecurity.csrf().disable();
+		httpSecurity.headers().frameOptions().disable();
+		
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+}
